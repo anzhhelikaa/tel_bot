a8d4f85d914913f364e5630daf1cb8a002ae1275
anzhhelikaa <anzhelikaostrianska@gmail.com>
2024-12-19 16:05:51 +0100

    Add files via upload

---
+++ bot_logic.py
@@ -0,0 +1,15 @@
+import random
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "ОРЕЛ"
+    else:
+        return "РЕШКА"
+    
+
+    
+
+
+
---
+++ config.py
@@ -0,0 +1,1 @@
+token = '7510753713:AAEf9aZZ4YkCZLOjr5Cc54pvtuHJwP6OxiM'
